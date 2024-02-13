1953cc41bf9f6271dedf3e089c9bc820df625b5f
harini1503-git <155567631+harini1503-git@users.noreply.github.com>
2024-02-13 19:40:56 +0530

    Add files via upload

---
+++ Flutter/flutter_application_1-1/demo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class dashboard1 extends StatelessWidget {
+  const dashboard1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+    SafeArea( 
+      child: Scaffold(
+        
+        body: Center(
+          child: Text("Mudaliar Harini Ravi")),
+    
+    
+        ),
+    );
+   }
+}
